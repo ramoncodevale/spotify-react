7b36884a7cf584421383cd2a8e823737dc4ecfd8
ramoncodevale <ramonvalentim88@gmail.com>
2025-02-21 17:01:45 -0300

    ajuste no process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,8 +1,7 @@
 import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+const URL = "https://spotify-react-deef.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
