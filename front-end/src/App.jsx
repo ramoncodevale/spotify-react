600e9fe5b123a5042365c33e449561a6c67e70cd
ramoncodevale <ramonvalentim88@gmail.com>
2025-02-21 16:50:51 -0300

    corrigindo o nome do arquivo

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/header";
+import Header from "./components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Artists from "./pages/Artists";
